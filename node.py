82a8dc0f0d361d5e98bd7b0bc83ac87d80416775
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-07-25 15:10:27 -0400

    first commit

---
+++ node.py
@@ -0,0 +1,8 @@
+'''this will be used for as the node class for all of the container classes'''
+
+class Node:
+  def __init__(self,data= None,next = None):
+    self.data = data
+    self.next = next
+    self.left = None
+    self.right = None
